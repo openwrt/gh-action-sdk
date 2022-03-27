openwrt/gh-action-sdk

commit d29e7cc00b266b489491a0c04663562780268063

Use feeds from GH mirror for GH actions
In order to lower resource usage.

Signed-off-by: Petr Štetiar <ynezz@true.cz>

entrypoint.sh
@@ -15,7 +15,10 @@ fi
 echo "src-link $FEEDNAME $GITHUB_WORKSPACE/" > feeds.conf
 
 if [ -z "$NO_DEFAULT_FEEDS" ]; then
-	cat feeds.conf.default >> feeds.conf
+	echo 'src-git-full luci https://github.com/openwrt/luci' > feeds.conf
+	echo 'src-git-full routing https://github.com/openwrt/routing' >> feeds.conf
+	echo 'src-git-full packages https://github.com/openwrt/packages' >> feeds.conf
+	echo 'src-git-full telephony https://github.com/openwrt/telephony' >> feeds.conf
 fi
 
 #shellcheck disable=SC2153

commit 2cf367441cce736879cede58c6df63bab959839f

Fix feeds.conf
d29e7cc00b26 introduced a number of issues:

* The src-link line for $GITHUB_WORKSPACE in feeds.conf is overridden.

* The base feed is missing.

* Any branch/tag/commit information in feeds.conf.default is omitted,
  i.e. each feed's master branch is always used.

This fixes all of these issues.

Fixes: d29e7cc00b26 ("Use feeds from GH mirror for GH actions")
Signed-off-by: Jeffery To <jeffery.to@gmail.com>

entrypoint.sh
@@ -15,10 +15,11 @@ fi
 echo "src-link $FEEDNAME $GITHUB_WORKSPACE/" > feeds.conf
 
 if [ -z "$NO_DEFAULT_FEEDS" ]; then
-	echo 'src-git-full luci https://github.com/openwrt/luci' > feeds.conf
-	echo 'src-git-full routing https://github.com/openwrt/routing' >> feeds.conf
-	echo 'src-git-full packages https://github.com/openwrt/packages' >> feeds.conf
-	echo 'src-git-full telephony https://github.com/openwrt/telephony' >> feeds.conf
+	sed \
+		-e 's,https://git.openwrt.org/feed/,https://github.com/openwrt/,' \
+		-e 's,https://git.openwrt.org/openwrt/,https://github.com/openwrt/,' \
+		-e 's,https://git.openwrt.org/project/,https://github.com/openwrt/,' \
+		feeds.conf.default >> feeds.conf
 fi
 
 #shellcheck disable=SC2153