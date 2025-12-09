652d3e42db67e8b2ccfba5f05a8d2db1a47bf729
George Sapkin <george@sapk.in>
2025-12-10 01:16:33 +0200

    entypoint: make build optional
    Make build optional to enable running package and patch checks on
    formality failures without doing the heavier build.
    
    Don't hide relevant output under groups. GitHub doesn't allow
    auto-expanding groups on error.
    
    Fix nonexistent echoes.
    
    Signed-off-by: George Sapkin <george@sapk.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,8 @@ group "bash setup.sh"
 endgroup
 
 FEEDNAME="${FEEDNAME:-action}"
+# Build requested packages by default, otherwise just check
+BUILD="${BUILD:-1}"
 BUILD_LOG="${BUILD_LOG:-1}"
 
 if [ -n "$KEY_BUILD" ]; then
@@ -100,19 +102,21 @@ else
 			"package/$PKG/download" V=s
 		endgroup
 
-		group "make package/$PKG/check"
+		[ "$BUILD" = '1' ] && group "make package/$PKG/check"
 		make \
 			BUILD_LOG="$BUILD_LOG" \
 			IGNORE_ERRORS="$IGNORE_ERRORS" \
 			"package/$PKG/check" V=s 2>&1 | \
 				tee logtmp
-		endgroup
 
 		RET=${PIPESTATUS[0]}
+		[ "$BUILD" = '1' ] && endgroup
 
 		if [ "$RET" -ne 0 ]; then
-			echo_red   "=> Package check failed: $RET)"
+			echo 'Package check failed'
 			exit "$RET"
+		elif [ "$BUILD" = 0 ]; then
+			echo 'Package check successful'
 		fi
 
 		badhash_msg="HASH does not match "
@@ -125,12 +129,12 @@ else
 
 		PATCHES_DIR=$(find /feed -path "*/$PKG/patches")
 		if [ -d "$PATCHES_DIR" ] && [ -z "$NO_REFRESH_CHECK" ]; then
-			group "make package/$PKG/refresh"
+			[ "$BUILD" = '1' ] && group "make package/$PKG/refresh"
 			make \
 				BUILD_LOG="$BUILD_LOG" \
 				IGNORE_ERRORS="$IGNORE_ERRORS" \
 				"package/$PKG/refresh" V=s
-			endgroup
+			[ "$BUILD" = '1' ] && endgroup
 
 			if ! git -C "$PATCHES_DIR" diff --quiet -- .; then
 				echo "Dirty patches detected, please refresh and review the diff"
@@ -155,8 +159,12 @@ else
 				exit 1
 			fi
 		fi
-
 	done
+
+	if [ "$BUILD" != '1' ]; then
+		echo 'Skipping build'
+		exit
+	fi
 
 	make \
 		-f .config \
